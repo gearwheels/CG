02c08d0c9692c7b48ebbc14b0bee9dec5ebe0d78
gearwheels <TImofeevAV8f@yandex.ru>
2022-01-30 19:25:10 +0300

    ready

---
+++ mylabs/mylab1/Program.cs
@@ -0,0 +1,214 @@
+﻿// Тимофеев Алексей М8О-307Б-19 Вариант 3
+//#define UseOpenGL // Раскомментировать для использования OpenGL
+#if (!UseOpenGL)
+using Device     = CGLabPlatform.GDIDevice;
+using DeviceArgs = CGLabPlatform.GDIDeviceUpdateArgs;
+#else
+using Device     = CGLabPlatform.OGLDevice;
+using DeviceArgs = CGLabPlatform.OGLDeviceUpdateArgs;
+using SharpGL;
+#endif
+
+using System;
+using System.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+using CGLabPlatform;
+// ==================================================================================
+
+
+/*
+1) перемещение (сдвиг)
+2) масштабирование
+3) вращение
++ помимо этого
+4) авто масштабирование
+5) апроксимация
+ */
+using CGApplication = MyApp;
+public abstract class MyApp: CGApplicationTemplate<CGApplication, Device, DeviceArgs> {
+    // TODO: Добавить свойства, поля
+    public const int width = 300; // VSPanelWidth ширина поля с кнопками
+
+    [DisplayNumericProperty(Default: 100, Increment: 1, Minimum: 0, Maximum: 1000, Name: "Постоянная А ")]
+    public abstract double A { get; set; }
+
+    [DisplayNumericProperty(Default: 1000, Increment: 1, Minimum: 1, Maximum: 10000, Name: "Аппроксимация ")]
+    public abstract int VertexCount { get; set; }
+
+    [DisplayNumericProperty(Default: 2, Increment: 0.5, Minimum: 0.1, Maximum: 10, Name: "Zoom ")]
+    public abstract double zoom { get; set; }
+
+    [DisplayNumericProperty(Default: 0, Increment: 0.05, Minimum: 0, Maximum: 2, Name: "Коэффициент поворота*(pi) ")]
+    public abstract double angel { get; set; }
+
+    protected DVector2 Rotation(DVector2 point)
+    {
+        DVector2 resPoint;
+        //X = (x — x0) * cos(alpha) — (y — y0) * sin(alpha) + x0;
+        //Y = (x — x0) * sin(alpha) + (y — y0) * cos(alpha) + y0;
+        var cosA = Math.Cos(angel * Math.PI);
+        var sinA = Math.Sin(angel * Math.PI);
+        resPoint.X = (point.X - CenterFig.X) * cosA - (point.Y - CenterFig.Y) * sinA + CenterFig.X;
+        resPoint.Y = (point.X - CenterFig.X) * sinA + (point.Y - CenterFig.Y) * cosA + CenterFig.Y;
+        return resPoint;
+    }
+    
+    public  DVector2 Center; 
+    public  DVector2 CenterFig;
+    protected void CalculateCenter() // вычисление точки центра окна и точки центра фигуры 
+    {
+        Center = new DVector2(((base.MainWindow.Width - width) / 2) , (base.MainWindow.Height / 2) );
+        CenterFig = new DVector2( Shift.X,  - Shift.Y);
+        CenterFig.X *= (base.MainWindow.Height * base.MainWindow.Width) / (WindowWidth * WindowHeight);
+        CenterFig.Y *= (base.MainWindow.Height * base.MainWindow.Width) / (WindowWidth * WindowHeight);
+        CenterFig.X += Center.X;
+        CenterFig.Y += Center.Y;
+    }
+
+    protected double WindowWidth ;
+    protected double WindowHeight ;
+    protected DVector2 FromViewToPhysicalSpace(DVector2 point) {// Преобразование координат из видового пространства в физическое
+        double coeff ; // Коэффициент вычисляющийся из длинны и ширины окна. Используется для корректировки размера граффика
+        WindowWidth = 2500;
+        WindowHeight = 1380;
+        this.CalculateCenter();
+        DVector2 result = new DVector2(point.X , -point.Y ); // В физическом пространстве ось X направлена вправо, а Y направлена вниз
+        coeff = (base.MainWindow.Height * base.MainWindow.Width) / (WindowWidth * WindowHeight) ;
+        result.X *= coeff;
+        //coeff = (base.MainWindow.Height * base.MainWindow.Width)/ (WindowHeight * WindowWidth);
+        result.Y *= coeff;
+        result.X += Center.X;
+        result.Y += Center.Y;
+        return result;
+    }
+    
+    public abstract DVector2 Shift { get; set; }
+
+    public double delta;
+    protected override void OnMainWindowLoad(object sender, EventArgs args)
+    {
+        // Созданное приложение имеет два основных элемента управления:
+        // base.RenderDevice - левая часть экрана для рисования
+        // base.ValueStorage - правая панель для отображения и редактирования свойств
+
+        // Пример изменения внешниго вида элементов управления (необязательный код)
+        base.RenderDevice.BufferBackCol = 0x20;
+        base.ValueStorage.Font = new Font("Arial", 12f);
+        base.ValueStorage.ForeColor = Color.Firebrick;
+        base.ValueStorage.RowHeight = 30;
+        base.ValueStorage.BackColor = Color.BlanchedAlmond;
+        base.MainWindow.BackColor = Color.DarkGoldenrod;
+        base.ValueStorage.RightColWidth = 50;
+        base.VSPanelWidth = width;
+        base.VSPanelLeft = true;
+        base.MainWindow.Size = new Size(2500, 1380);
+        base.MainWindow.StartPosition = FormStartPosition.Manual;
+        base.MainWindow.Location = Point.Empty;
+
+        base.RenderDevice.GraphicsHighSpeed = false;
+        
+        
+        // Реализация управления мышкой с зажатыми левой и правой кнопкой мыши
+        base.RenderDevice.MouseMoveWithRightBtnDown += (s, e)
+            => Shift += 10 * new DVector2(e.MovDeltaX, -e.MovDeltaY);
+        base.RenderDevice.MouseMoveWithLeftBtnDown += (s, e)
+            => Shift +=  0.5 * new DVector2(e.MovDeltaX, -e.MovDeltaY);
+
+        //base.RenderDevice.MouseMoveWithMiddleBtnDown += (s, e)
+         //   => angel += e.MovDeltaX;
+
+        // Реализация управления клавиатурой
+        RenderDevice.HotkeyRegister(Keys.Up,    (s, e) => Shift += DVector2.UnitY);
+        RenderDevice.HotkeyRegister(Keys.Down,  (s, e) => Shift -= DVector2.UnitY);
+        RenderDevice.HotkeyRegister(Keys.Left,  (s, e) => Shift -= DVector2.UnitX);
+        RenderDevice.HotkeyRegister(Keys.Right, (s, e) => Shift += DVector2.UnitX);
+        RenderDevice.HotkeyRegister(KeyMod.Shift, Keys.Up,    (s, e) => Shift +=10*DVector2.UnitY);
+        RenderDevice.HotkeyRegister(KeyMod.Shift, Keys.Down,  (s, e) => Shift -=10*DVector2.UnitY);
+        RenderDevice.HotkeyRegister(KeyMod.Shift, Keys.Left,  (s, e) => Shift -=10*DVector2.UnitX);
+        RenderDevice.HotkeyRegister(KeyMod.Shift, Keys.Right, (s, e) => Shift +=10*DVector2.UnitX);
+
+        // ... расчет каких-то параметров или инициализация ещё чего-то, если нужно
+    }
+
+    protected void DrawScaleOY(DeviceArgs e) // разметка оси Y
+    {
+        var tmpCenterL = new DVector2(-10, 0);
+        var tmpCenterR = new DVector2(10, 0);
+        
+        for (int i = 0; i < 5; i++)
+        {
+            tmpCenterL.Y += 30;
+            tmpCenterR.Y += 30;
+            e.Surface.DrawLine(Color.Red.ToArgb(), FromViewToPhysicalSpace(tmpCenterL *zoom + Shift), FromViewToPhysicalSpace(tmpCenterR *zoom + Shift));
+        }
+        tmpCenterL.Y = 0;
+        tmpCenterR.Y = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            tmpCenterL.Y -= 30;
+            tmpCenterR.Y -= 30;
+            e.Surface.DrawLine(Color.Red.ToArgb(), FromViewToPhysicalSpace(tmpCenterL *zoom + Shift), FromViewToPhysicalSpace(tmpCenterR *zoom + Shift));
+        }
+    }
+    
+    protected void DrawScaleOX(DeviceArgs e)// разметка оси Х
+    {
+        var tmpCenterL = new DVector2(0, -10);
+        var tmpCenterR = new DVector2(0, 10);
+        
+        for (int i = 0; i < 5; i++)
+        {
+            tmpCenterL.X += 30;
+            tmpCenterR.X += 30;
+            e.Surface.DrawLine(Color.Red.ToArgb(), FromViewToPhysicalSpace(tmpCenterL *zoom + Shift), FromViewToPhysicalSpace(tmpCenterR *zoom + Shift));
+        }
+        tmpCenterL.X = 0;
+        tmpCenterR.X = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            tmpCenterL.X -= 30;
+            tmpCenterR.X -= 30;
+            e.Surface.DrawLine(Color.Red.ToArgb(), FromViewToPhysicalSpace(tmpCenterL *zoom + Shift), FromViewToPhysicalSpace(tmpCenterR *zoom + Shift));
+        }
+    }
+
+    protected override void OnDeviceUpdate(object s, DeviceArgs e) {
+        // TODO: Отрисовка и обновление
+        double step = 2 * Math.PI / VertexCount;
+        double angle = 0;
+        double X, Y;
+        //создаем OX, OY и разметку 
+        DVector2 mOX = new DVector2(-800, 0);
+        DVector2 OX = new DVector2(800, 0);
+        DVector2 mOY = new DVector2(0, -800);
+        DVector2 OY = new DVector2(0, 800);
+        e.Surface.DrawLine(Color.Red.ToArgb(), FromViewToPhysicalSpace(mOX *zoom + Shift), FromViewToPhysicalSpace(OX *zoom + Shift));
+        e.Surface.DrawLine(Color.Red.ToArgb(), FromViewToPhysicalSpace(mOY *zoom + Shift), FromViewToPhysicalSpace(OY *zoom + Shift));
+        this.DrawScaleOY(e);
+        this.DrawScaleOX(e);
+        //создали OX, OY и разметку
+        List<DVector2> points = new List<DVector2>();
+        while (angle < 2 * Math.PI) { // просчет точек графика
+            X = A *  Math.Pow(Math.Cos(angle),3) *zoom;
+            Y = A * Math.Pow (Math.Sin(angle),3) *zoom;
+            points.Add(new DVector2(X, Y)+Shift); 
+            angle += step;
+        }
+        X = A *  Math.Pow(Math.Cos(angle),3) *zoom;
+        Y = A * Math.Pow (Math.Sin(angle),3) *zoom;
+        points.Add(new DVector2(X, Y)+Shift);
+
+        for (int i = 1; i < points.Count; ++i) { // рисуем график 
+            e.Surface.DrawLine(Color.LawnGreen.ToArgb(), Rotation(FromViewToPhysicalSpace(points[i])), Rotation(FromViewToPhysicalSpace(points[i - 1])));
+        }
+      
+    }
+
+}
+
+// ==================================================================================
+public abstract class AppMain : CGApplication
+{ [STAThread] static void Main() { RunApplication(); } }
---
+++ mylabs/mylab2/Program.cs
@@ -0,0 +1,435 @@
+﻿//#define UseOpenGL // Раскомментировать для использования OpenGL
+#if (!UseOpenGL)
+using Device     = CGLabPlatform.GDIDevice;
+using DeviceArgs = CGLabPlatform.GDIDeviceUpdateArgs;
+#else
+using Device     = CGLabPlatform.OGLDevice;
+using DeviceArgs = CGLabPlatform.OGLDeviceUpdateArgs;
+using SharpGL;
+#endif
+
+using System;
+using System.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using CGLabPlatform;
+// ==================================================================================
+
+
+using CGApplication = MyApp;
+using System.ComponentModel;
+
+public abstract class MyApp: CGApplicationTemplate<CGApplication, Device, DeviceArgs>
+{
+    public const int width = 400; // VSPanelWidth ширина поля с кнопками
+    public class Vertex
+    {
+        private List<Polygon> polygons;
+        public Vertex() { }
+        public Vertex(double[] elements)
+        {
+            Debug.Assert(elements != null && elements.Length == 4);
+            this.Point_InLocalSpace.X = elements[0];
+            this.Point_InLocalSpace.Y = elements[1];
+            this.Point_InLocalSpace.Z = elements[2];
+            this.Point_InLocalSpace.W = elements[3];
+        }
+        public DVector4 Point_InLocalSpace;
+        public DVector4 Point_InWorldSpace;
+    }
+    public class Polygon
+    {
+        public Vertex[] vertecies;
+        public DVector4 Normal_InLocalSpace;
+        public DVector4 Normal_InWorldSpace;
+        public Polygon() { }
+        public Polygon(Vertex[] elements)
+        {
+            Debug.Assert(elements != null && elements.Length == 3);
+            vertecies = elements;
+        }
+        public bool IsVisible;
+        public int Color;
+    }
+
+
+    // TODO: Добавить свойства, поля
+    private DMatrix4 _PointTransform;
+    private Commands _Commands = Commands.FigureChange;
+    private DVector3 _Rotation;
+    private DVector3 _Offset;
+    private DVector3 _Scale;
+    
+    [Flags]
+    private enum Commands : int
+    {
+        None = 0,
+        Transform = 1 << 0,
+        FigureChange = 1 << 1
+    }
+    
+    [DisplayNumericProperty(Default: new[] { 1d, 0, 0, 0,
+                                             0, 1d, 0, 0,
+                                             0, 0, 1d, 0, 
+                                             0, 0, 0, 1d }, 0.01, 4,  null)]
+    public DMatrix4 PointTransform 
+    {
+        get { return _PointTransform; }
+        set {
+            if (value == _PointTransform)
+                return;
+            _PointTransform = value;
+            _Commands |= Commands.Transform;
+            OnPropertyChanged();
+        }
+    }
+
+
+    [DisplayNumericProperty(Default: 100, Increment: 0.01, Minimum: 1, Name: "Радиус основания")]
+    public double BaseRadius
+    {
+        get { return Get<double>(); }
+        set
+        {
+            if (Set<double>(value))
+            {
+                _Commands |= Commands.FigureChange;
+            }
+        }
+    }
+
+    [DisplayNumericProperty(Default: 100, Increment: 0.01, Minimum: 1, Name: "Высота пирамиды")]
+    public double PyrHeight
+    {
+        get { return Get<double>(); }
+        set
+        {
+            if (Set<double>(value))
+            {
+                _Commands |= Commands.FigureChange;
+            }
+        }
+    }
+
+    [DisplayNumericProperty(Default: new[] { 0d, 0d, 0d }, Increment: 0.1, Name: "Смещение")]
+    public DVector3 Offset
+    {
+        get { return _Offset; }
+        set {
+            if (Set<DVector3>(value))
+            {
+                _Offset = value;
+                UpdateTransformMatrix();
+            }
+        }
+    }
+
+    [DisplayNumericProperty(Default: new[] { 0d, 0d, 0d },  Increment: 1, Name: "Поворот")]
+    public DVector3 Rotation 
+    {
+        get { return _Rotation; }
+        set {
+            if (Set<DVector3>(value))
+            {
+                _Rotation = value;
+                if (_Rotation.X >= 360) _Rotation.X -= 360;
+                if (_Rotation.Y >= 360) _Rotation.Y -= 360;
+                if (_Rotation.Z >= 360) _Rotation.Z -= 360;
+                if (_Rotation.X < 0) _Rotation.X += 360;
+                if (_Rotation.Y < 0) _Rotation.Y += 360;
+                if (_Rotation.Z < 0) _Rotation.Z += 360;
+                UpdateTransformMatrix();
+            }
+        }
+    }
+
+    [DisplayNumericProperty(Default: new[] { 20d, 20d, 20d }, Increment: 0.1, Name: "Масштаб")]
+    public DVector3 Scale
+    {
+        get { return _Scale; }
+        set
+        {
+            _Scale = value;
+            if (Set<DVector3>(value))
+            {
+                UpdateTransformMatrix();
+            }
+        }
+    }
+
+    public enum Projection
+    {
+        [Description("Не задана")] NotSet,
+        [Description("Вид спереди")] InFront,
+        [Description("Вид сверху")] Above,
+        [Description("Вид сбоку")] Sideway,
+        [Description("Изометрия")] Isometry
+    }
+
+    [DisplayEnumListProperty(Projection.NotSet, Name: "Проекция")]
+    public Projection CurProjection 
+    {
+        get { return Get<Projection>(); }
+        set
+        {
+            if (Set<Projection>(value))
+            {
+                if (CurProjection == Projection.Above)
+                {
+                    _Scale.Y = 0;
+                }
+                else if (CurProjection == Projection.InFront)
+                {
+                    _Scale.Z = 0;
+                }
+                else if (CurProjection == Projection.Sideway)
+                {
+                    _Scale.X = 0;
+                }
+                else if (CurProjection == Projection.Isometry)
+                {
+                    _Rotation.X = 35;
+                    _Rotation.Y = 45;
+                    _Rotation.Z = 0;
+                }
+                UpdateTransformMatrix();
+                _Commands |= Commands.Transform;
+            }
+        }
+    }
+
+    public enum Visualization
+    {
+        [Description("Не отображать полигоны")] NoPolygons,
+        [Description("Одним цветом")] OneColor,
+        [Description("Случайные цвета")] RandomColor
+    }
+    [DisplayEnumListProperty(Visualization.NoPolygons, Name: "Способ отрисовки")]
+    public abstract Visualization CurVisual { get; set; }
+
+    [DisplayCheckerProperty(Default: true, Name: "Каркасная визуализация")]
+    public abstract bool IsCarcass { get; set; }
+
+    public DMatrix4 NormalTransform;
+
+    public Vertex[] vertices;
+    public Polygon[] polygons;
+
+    public DVector2 ChangePos;
+    public DVector2 ChangeAngle;
+
+    public DVector2 ViewSize;
+    public DVector2 Automove;
+    public DVector2 AutoScale;
+    
+    private void UpdateTransformMatrix()
+    {
+        _PointTransform = new DMatrix4(new double[] {1, 0, 0, 0,
+                                                    0, 1, 0, 0,
+                                                    0, 0, 1, 0,
+                                                    0, 0, 0, 1 });
+        
+        /* Смещение */
+        DMatrix4 offset = new DMatrix4(new double[] { 1, 0, 0, _Offset.X,
+            0, 1, 0, _Offset.Y,
+            0, 0, 1, _Offset.Z ,
+            0, 0, 0, 1 });
+        _PointTransform *= offset;
+        
+        /* Поворот */
+        // По оси X
+        DMatrix4 x_rotate = new DMatrix4(new double[] {1, 0, 0, 0,
+                                                       0, Math.Cos(Math.PI / 180 * _Rotation.X), Math.Sin(Math.PI / 180 * _Rotation.X), 0,
+                                                       0, -Math.Sin(Math.PI / 180 * _Rotation.X), Math.Cos(Math.PI / 180 * _Rotation.X), 0,
+                                                       0, 0, 0, 1 });
+        DMatrix4 y_rotate = new DMatrix4(new double[] {Math.Cos(Math.PI / 180 * _Rotation.Y), 0, Math.Sin(Math.PI / 180 * _Rotation.Y), 0,
+                                                        0, 1, 0, 0,
+                                                       -Math.Sin(Math.PI / 180 * _Rotation.Y), 0, Math.Cos(Math.PI / 180 * _Rotation.Y), 0,
+                                                        0, 0, 0, 1 });
+        DMatrix4 z_rotate = new DMatrix4(new double[] {Math.Cos(Math.PI / 180 * _Rotation.Z), -Math.Sin(Math.PI / 180 * _Rotation.Z), 0, 0,
+                                                        Math.Sin(Math.PI / 180 * _Rotation.Z), Math.Cos(Math.PI / 180 * _Rotation.Z), 0, 0,
+                                                        0, 0, 1, 0,
+                                                        0, 0, 0, 1 });
+        _PointTransform *= x_rotate;
+        _PointTransform *= y_rotate;
+        _PointTransform *= z_rotate;
+
+        /* Маштабирование */
+        _PointTransform *= new DMatrix4(new double[] {_Scale.X, 0, 0, 0,
+                                                        0, _Scale.Y, 0, 0,
+                                                        0, 0, _Scale.Z, 0 ,
+                                                        0, 0, 0, 1 });
+
+        _Commands |= Commands.Transform;
+        
+    }
+    public void Create()
+    {
+        int VertexSize = 6;
+        int PolygonSize = 8;
+        vertices = new Vertex[VertexSize]; //6
+        polygons = new Polygon[PolygonSize]; //8
+        var random = new Random();
+        for (int i = 0; i < vertices.Length; ++i)
+        {
+            vertices[i] = new Vertex();
+        }
+        for (int i = 0; i < polygons.Length; ++i)
+        {
+            polygons[i] = new Polygon();
+            polygons[i].Color = random.Next();
+        }
+        Generate();
+    }
+    public void Generate()
+    {
+        int numPoint = 5;
+        for (int i = 0; i <numPoint - 1; ++i)
+        {
+            vertices[i].Point_InLocalSpace.X = BaseRadius * Math.Cos(2 * i * Math.PI / (numPoint - 1));
+            vertices[i].Point_InLocalSpace.Y = BaseRadius * Math.Sin(2 * i * Math.PI / (numPoint - 1));
+            vertices[i].Point_InLocalSpace.Z = 0.01; 
+            vertices[i].Point_InLocalSpace.W = 1;
+        }
+        vertices[numPoint - 1].Point_InLocalSpace.X = 0;
+        vertices[numPoint - 1].Point_InLocalSpace.Y = 0;
+        vertices[numPoint - 1].Point_InLocalSpace.Z = -1*PyrHeight ;
+        vertices[numPoint - 1].Point_InLocalSpace.W = 1;
+        
+        vertices[numPoint].Point_InLocalSpace.X = 0;
+        vertices[numPoint].Point_InLocalSpace.Y = 0;
+        vertices[numPoint].Point_InLocalSpace.Z =  PyrHeight;
+        vertices[numPoint].Point_InLocalSpace.W = 1;
+        
+        polygons[0].vertecies = new []{vertices[numPoint - 1], vertices[0], vertices[1] };
+        polygons[1].vertecies = new []{vertices[numPoint - 1], vertices[1], vertices[2] };
+        polygons[2].vertecies = new []{vertices[numPoint - 1], vertices[2], vertices[3] };
+        polygons[3].vertecies = new []{vertices[numPoint - 1], vertices[3], vertices[0] };
+        polygons[4].vertecies = new []{vertices[1 ], vertices[0], vertices[numPoint] };
+        polygons[5].vertecies = new []{vertices[2 ], vertices[1], vertices[numPoint] };
+        polygons[6].vertecies = new []{vertices[3 ], vertices[2], vertices[numPoint] };
+        polygons[7].vertecies = new []{vertices[0 ], vertices[3], vertices[numPoint] };
+        
+        foreach (var p in polygons)
+        {
+            DVector4 first = new DVector4(new double[] {p.vertecies[1].Point_InLocalSpace.X - p.vertecies[0].Point_InLocalSpace.X,
+                                                        p.vertecies[1].Point_InLocalSpace.Y - p.vertecies[0].Point_InLocalSpace.Y,
+                                                        p.vertecies[1].Point_InLocalSpace.Z - p.vertecies[0].Point_InLocalSpace.Z,
+                                                        p.vertecies[1].Point_InLocalSpace.W - p.vertecies[0].Point_InLocalSpace.W});
+            DVector4 second = new DVector4(new double[] {p.vertecies[2].Point_InLocalSpace.X - p.vertecies[0].Point_InLocalSpace.X,
+                                                        p.vertecies[2].Point_InLocalSpace.Y - p.vertecies[0].Point_InLocalSpace.Y,
+                                                        p.vertecies[2].Point_InLocalSpace.Z - p.vertecies[0].Point_InLocalSpace.Z,
+                                                        p.vertecies[2].Point_InLocalSpace.W - p.vertecies[0].Point_InLocalSpace.W});
+            p.Normal_InLocalSpace = first * second;
+            p.Normal_InLocalSpace.Normalize();
+        }
+
+    }
+    protected DVector2 FromViewToPhysicalSpace(Vertex point)
+    {
+        DVector2 result = new DVector2();
+        result.X = point.Point_InWorldSpace.X  / point.Point_InWorldSpace.W;
+        result.Y = point.Point_InWorldSpace.Y / point.Point_InWorldSpace.W;
+        result.X = result.X * AutoScale.X + Automove.X; // Преобразование координат из видового пространства в физическое
+        result.Y = result.Y * AutoScale.Y + Automove.Y;
+        return result;
+    }
+
+    protected override void OnMainWindowLoad(object sender, EventArgs args)
+    {
+        // TODO: Инициализация данных
+        base.RenderDevice.BufferBackCol = 0x20;
+        base.ValueStorage.Font = new Font("Arial", 12f);
+        base.ValueStorage.ForeColor = Color.Firebrick;
+        base.ValueStorage.RowHeight = 30;
+        base.ValueStorage.BackColor = Color.BlanchedAlmond;
+        base.MainWindow.BackColor = Color.DarkGoldenrod;
+        base.ValueStorage.RightColWidth = 50;
+        base.VSPanelWidth = width;
+        base.VSPanelLeft = true;
+        base.MainWindow.Size = new Size(2500, 1380);
+        base.MainWindow.StartPosition = FormStartPosition.Manual;
+        base.MainWindow.Location = Point.Empty;
+
+        base.RenderDevice.GraphicsHighSpeed = false;
+        base.RenderDevice.BufferBackCol = 0x20;
+
+        base.RenderDevice.MouseMoveWithRightBtnDown += (s, e)
+        => Offset += new DVector3(0.35*Math.Abs(_Scale.X)*e.MovDeltaX, 0.35*Math.Abs(_Scale.Y)*e.MovDeltaY, 0);
+        base.RenderDevice.MouseMoveWithLeftBtnDown += (s, e)
+        => Rotation += new DVector3(0.1 * e.MovDeltaY, 0.1 * e.MovDeltaX , 0);
+        base.RenderDevice.MouseWheel += (s, e) => Scale += new DVector3(0.05 * e.Delta, 0.05 * e.Delta, 0.05 * e.Delta);
+        Create();
+    }
+    
+    protected override void OnDeviceUpdate(object s, DeviceArgs e)
+    {
+        if (0 != ((int)_Commands & (int)Commands.FigureChange))
+        {
+            _Commands ^= Commands.FigureChange;
+            Generate();
+        }
+
+        /* Обновление значений, использующихся для перевода в физ. с. к. */
+        var x_min = vertices.Min(p => (p.Point_InLocalSpace.X / p.Point_InLocalSpace.Z));
+        var x_max = vertices.Max(p => (p.Point_InLocalSpace.X / p.Point_InLocalSpace.Z));
+        var y_min = vertices.Min(p => (p.Point_InLocalSpace.Y / p.Point_InLocalSpace.Z));
+        var y_max = vertices.Max(p => (p.Point_InLocalSpace.Y / p.Point_InLocalSpace.Z));
+        ViewSize.X = x_max - x_min;
+        ViewSize.Y = y_max - y_min;
+        AutoScale.X = .9 * e.Width / ViewSize.X;
+        AutoScale.Y = .9 * e.Heigh / ViewSize.Y;
+        AutoScale.X = AutoScale.Y = Math.Min(AutoScale.X, AutoScale.Y);
+        Automove.X = e.Width / 2 - (x_min + x_max) / 2 * AutoScale.X;
+        Automove.Y = e.Heigh / 2 - (y_min + y_max) / 2 * AutoScale.Y;
+
+        if (0 != ((int) _Commands & (int) Commands.Transform))
+        {
+            _Commands ^= Commands.Transform;
+            // Пересчет преобразования вектора нормали
+            NormalTransform = DMatrix3.NormalVecTransf(PointTransform);
+
+            foreach (var v in vertices)
+            {
+                v.Point_InWorldSpace = PointTransform * v.Point_InLocalSpace;
+            }
+
+            foreach (var p in polygons)
+            {
+                p.Normal_InWorldSpace = NormalTransform * p.Normal_InLocalSpace;
+                p.IsVisible = p.Normal_InWorldSpace.Z < 0;
+            }
+            polygons.OrderBy(p => Math.Min(p.vertecies[0].Point_InWorldSpace.Z, Math.Min(p.vertecies[1].Point_InWorldSpace.Z, p.vertecies[2].Point_InWorldSpace.Z)));
+        }
+        
+        foreach (var p in polygons)
+        {
+            if (!p.IsVisible)
+                continue;
+
+            if (CurVisual == Visualization.OneColor)
+            {
+                e.Surface.DrawTriangle(Color.YellowGreen.ToArgb(), FromViewToPhysicalSpace(p.vertecies[0]), FromViewToPhysicalSpace(p.vertecies[1]),
+                    FromViewToPhysicalSpace(p.vertecies[2]));
+            }
+            else if (CurVisual == Visualization.RandomColor)
+            {
+                e.Surface.DrawTriangle(p.Color, FromViewToPhysicalSpace(p.vertecies[0]), FromViewToPhysicalSpace(p.vertecies[1]),
+                    FromViewToPhysicalSpace(p.vertecies[2]));
+            }
+
+            if (IsCarcass)
+            {
+                e.Surface.DrawLine(Color.Green.ToArgb(), FromViewToPhysicalSpace(p.vertecies[0]), FromViewToPhysicalSpace(p.vertecies[1]));
+                e.Surface.DrawLine(Color.Green.ToArgb(), FromViewToPhysicalSpace(p.vertecies[1]), FromViewToPhysicalSpace(p.vertecies[2]));
+                e.Surface.DrawLine(Color.Green.ToArgb(), FromViewToPhysicalSpace(p.vertecies[2]), FromViewToPhysicalSpace(p.vertecies[0]));
+            }
+        }
+    }
+}
+// ==================================================================================
+public abstract class AppMain : CGApplication
+{ [STAThread] static void Main() { RunApplication(); } }
